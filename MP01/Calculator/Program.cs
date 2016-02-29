3089f77d39a57e985913444bd100e9c0812b971f
i.vasilev <vanvas62@gmail.com>
2016-02-29 15:57:05 +0200

    Модул 06, Видео 03 - архитектура

--- MP01/Calculator/Program.cs
+++ MP01/Calculator/Program.cs
@@ -3,10 +3,15 @@
 namespace Calculator
 {
 	class MainClass
-	{
+		{
+		//Дефиниране на библиотека
+		public UserInput.UInput  uImput = new UserInput.UInput ();
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+			MainClass _program = new MainClass ();
+			_program.uImput.justTesting ();
 		}
 	}
 }
