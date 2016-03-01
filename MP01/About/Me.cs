ecb7b20b18a42ca0ce7abf6d18de3264143bee0c
i.vasilev <vanvas62@gmail.com>
2016-03-01 16:35:10 +0200

    Модул 06, видео 05 - Библиотека инфо за програмата

---
+++ MP01/About/Me.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace About
+{
+	public class Me
+	{
+		public string shortName = "СКлакулатор";
+		public string longName = "Строителен калкулатор за изкопни работи";
+
+		public string version = "вер. 1.00";
+		public int versionNumber = 100;
+
+
+
+		public Me ()
+		{
+		}
+	}
+}
+
--- MP01/Calculator/Program.cs
+++ MP01/Calculator/Program.cs
@@ -6,14 +6,18 @@ namespace Calculator
 		{
 		//Дефиниране на библиотека
 		public UserInput.UInput  uImput = new UserInput.UInput ();
-
+		public About.Me about = new About.Me ();
 
 
 		public static void Main (string[] args)
 		{
 			MainClass _program = new MainClass ();
 
+			//Форматиране на програмата
+			Console.Title = _program.about.shortName + ", "  + _program.about.version;
 
+			//стартиране на програмата
+			_program.uImput.sayHello ();
 			_program.uImput.getUserCommands ();
 		}
 	}
